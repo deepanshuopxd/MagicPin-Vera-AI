31a480611c4234813dded845a70944699ec2af5a
deepanshuopxd <deepanshufulara17@gmail.com>
2026-09-17 02:57:05 +0530

    fixed error

--- bot/main.py
+++ bot/main.py
@@ -324,5 +324,11 @@ def teardown():
 if __name__ == "__main__":
     import uvicorn
     import os
-    port = int(os.environ.get("PORT", 8080))
-    uvicorn.run("main:app", host="0.0.0.0", port=port)
+
+    port = int(os.environ["PORT"])
+
+    uvicorn.run(
+        "main:app",
+        host="0.0.0.0",
+        port=port
+    )
