e4645531203691428bb2c4650714e2b7c7f09a7a
deepanshuopxd <deepanshufulara17@gmail.com>
2026-09-17 02:47:26 +0530

    fixed

--- bot/main.py
+++ bot/main.py
@@ -325,4 +325,4 @@ if __name__ == "__main__":
     import uvicorn
     import os
     port = int(os.environ.get("PORT", 8080))
-    uvicorn.run(app, host="0.0.0.0", port=port)
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
